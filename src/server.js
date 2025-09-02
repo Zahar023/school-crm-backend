951ab6979f8a94a4f0fe5fe872171c9becdf7da0
republik <zahar-023@mail.ru>
2025-09-02 20:25:12 +0700

    new cors

--- src/server.js
+++ src/server.js
@@ -10,7 +10,18 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://backend-school-crm.onrender.com",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
